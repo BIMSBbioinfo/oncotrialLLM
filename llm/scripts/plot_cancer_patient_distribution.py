50ef8009c7cd92a210dd78e8dd351e600dcde9e1
nouralkhoury <nour.alkhoury@lau.edu>
2024-07-22 19:08:44 +0200

    plot percentage instead of count

--- llm/scripts/plot_cancer_patient_distribution.py
+++ llm/scripts/plot_cancer_patient_distribution.py
@@ -1,7 +1,6 @@
 import hydra
 import pandas as pd
 import matplotlib.pyplot as plt
-
 import os
 
 @hydra.main(version_base=None, config_path="../conf", config_name="config")
@@ -11,25 +10,26 @@ def main(cfg):
     # Count patients per cancer type
     patient_counts = patients_with_biomarker['CANCER_TYPE'].value_counts()
 
-    # Select top 30 cancer types
-    top_30 = patient_counts.head(30)
+    # Calculate percentages
+    patient_percentage = (patient_counts / patient_counts.sum()) * 100
+
+    # Select top cancer types
+    top_percentage = patient_percentage.head(20)
 
     # Plotting with matplotlib
     plt.figure(figsize=(3.54,3.54), dpi=600)
-    top_30.plot(kind='bar', color='C0')
+    top_percentage.plot(kind='bar', color='C0')
     #plt.title('Top 30 Cancer Types by Number of Patients')
-    plt.xlabel('Cancer Type', fontsize=7)
-    plt.ylabel('Number of Patients', fontsize=7)
-    plt.xticks(rotation=45, ha='right', fontsize=5)
-    plt.yticks(fontsize=5)
+    plt.xlabel('Cancer Type', fontsize=8)
+    plt.ylabel('Percentage of Patients\nwith a biomarker (%)', fontsize=8)
+    plt.xticks(rotation=90, ha='right', fontsize=7)
+    plt.yticks(fontsize=7)
     plt.tight_layout()
 
     figures_dir = cfg.figures.dir
     if not os.path.exists(figures_dir):
         os.makedirs(figures_dir)
-    plt.savefig(f'{figures_dir}/aacr_patient_cancer_distribution.png', bbox_inches='tight')
-
-
+    plt.savefig(f'{figures_dir}/aacr_patient_cancer_distribution_perc.png')
 
 if __name__ == "__main__":
     main()
